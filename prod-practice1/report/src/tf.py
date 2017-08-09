b729711a54084ad88447c81362e517a5742ca464
Konstantin Gorbunov <konstunn@users.noreply.github.com>
2017-08-09 19:06:05 +0700

    Make tf while loop returning long sequence

--- prod-practice1/report/src/tf.py
+++ prod-practice1/report/src/tf.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python3
 
+# import numpy as np
 import tensorflow as tf
 
 n = 2
 k = 3
 
-F = tf.constant([-.1, 0, 0, -.1], tf.float64, [n, n], "system_matrix")
-print(F)
+F = tf.constant([.1, 0, 0, .1], tf.float64, [n, n], "system_matrix")
+C = tf.constant([1, 0, 0, 1], tf.float64, [2, 2], "control_matrix")
 
-x0 = tf.constant(1, tf.float64, [n, 1], "initial_state")
-print(x0)
+x = tf.constant(1, tf.float64, [n, 1], "state")
 
-x = tf.matmul(F, x0, name="state")
-print(x)
-
-u = tf.constant([1, 2, 3, 4, 5, 6], tf.float64, [n, k], "input")
+u = tf.constant(10, tf.float64, [n, k], "input")
 
 # i = tf.constant([0, 0], tf.int32, [2])
 # xdims = tf.constant([n, 1], tf.int32, [2])
 
 i = tf.constant(0, tf.int32)
+j = tf.constant([0, 0], tf.int32)
 
 
-c = lambda x, i: tf.less(i, k)
+def c(x, i, k):
+    return tf.less(i, k)
 
-def b(x, i):
-    x = tf.matmul(F, x)
+
+def b(x, i, k):
+    xs = tf.slice(x, [0, i], [2, 1])
+    xp = tf.matmul(F, xs)
+    x = tf.concat([x, xp], 1)
     i = i + 1
-    return x, i
+    return x, i, k
 
 
-loop = tf.while_loop(c, b, (x, i))
+loop = tf.while_loop(c, b, [x, i, k],
+                     [tf.TensorShape([2, None]), i.get_shape(),
+                      tf.TensorShape([])])
 
-# u1 = tf.slice(u, i, xdims)
+# us = tf.slice(u, i, xdims)
 
 with tf.Session() as sess:
-    print(sess.run(x))
+    # print(sess.run(x))
     print(sess.run(loop)[0])
     print(sess.run(u))
-    # print(sess.run(u1))
+    # print(sess.run(us))
